01c9ab5ff299aa20f700e37419f30336d8c82f4b
TotallyUselessCoder <rabbiarifurrahman@gmail.com>
2017-02-18 15:23:11 +0600

    Add files via upload

---
+++ Repeatetion Finder.c
@@ -0,0 +1,31 @@
+/* A program to find out how many times a character is repeated in a given string*/
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char data[100];
+    int x,y,count=0,lenght;
+    scanf("%[^\n]", &data);
+    lenght=strlen(data);
+    for(x=0;x<=lenght-1;x++)
+    {
+        if(data[x]=='\0')
+        {
+            continue;
+        }
+        for(y=0;y<=lenght-1;y++)
+        {
+            if(data[x]==data[y]&&x!=y)
+            {
+                count++;
+                data[y]='\0';
+            }
+        }
+        if(count>0)
+        {
+            printf("%c is repeated %d times\n", data[x],count);
+            count=0;
+        }
+    }
+    getch();
+}
